d1fc30a1c2e2d9b891587553ec4a88bda5736c10
shinji <shinbey221@gmail.com>
2019-09-23 17:52:55 +0900

    complete redux(not API)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux'
-import { Provider } from 'redux'
+import { Provider } from 'react-redux'
 import reducer from './reducers'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker';
