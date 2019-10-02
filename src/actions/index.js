7b7dbaf6232ca5d1057e75d7ff3ae85f173aa616
shinji <shinbey221@gmail.com>
2019-10-02 15:15:41 +0900

    add: delete on event_update

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
 export const CREATE_EVENT = 'CREATE_EVENT'
+export const DELETE_EVENT = 'DELETE_EVENT'
+export const UPDATE_EVENT = 'UPDATE_EVENT'
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -15,5 +17,11 @@ export const postEvent = values => async dispatch => {
     const response = await axios.post(`${ROOT_URL}/events${QUERYSTRING}`, values)
     dispatch({ type: CREATE_EVENT, response })
 }
+
+export const deleteEvent = id => async dispatch => {
+    await axios.delete(`${ROOT_URL}/events/${id}${QUERYSTRING}`)
+    dispatch({ type: DELETE_EVENT, id })
+}
+
 
 
